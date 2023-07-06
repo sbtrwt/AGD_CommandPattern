a4e9670b99a49f1f95d4509697f32421ba47a2f8
mridul-outscal <mridul.agrawal@outscal.com>
2023-07-06 15:33:07 +0530

    Added Event Architecture

---
+++ Assets/Scripts/Events/EventController.cs
@@ -0,0 +1,18 @@
+using System;
+
+/**  This script demonstrates implementation of the Observer Pattern.
+ *  If you're interested in learning about Observer Pattern, 
+ *  you can find a dedicated course on Outscal's website.
+ *  Link: https://outscal.com/courses
+ **/
+
+namespace Command.Events
+{
+    public class GameEventController<T>
+    {
+        public event Action<T> baseEvent;
+        public void InvokeEvent(T type) => baseEvent?.Invoke(type);
+        public void AddListener(Action<T> listener) => baseEvent += listener;
+        public void RemoveListener(Action<T> listener) => baseEvent -= listener;
+    }
+}
---
+++ Assets/Scripts/Events/EventService.cs
@@ -0,0 +1,19 @@
+/**  This script demonstrates implementation of the Observer Pattern.
+ *  If you're interested in learning about Observer Pattern, 
+ *  you can find a dedicated course on Outscal's website.
+ *  Link: https://outscal.com/courses
+ **/
+
+namespace Command.Events
+{
+    public class EventService
+    {
+        public GameEventController<int> OnMapSelected { get; private set; }
+
+        public EventService()
+        {
+            OnMapSelected = new GameEventController<int>();
+        }
+
+    }
+}
