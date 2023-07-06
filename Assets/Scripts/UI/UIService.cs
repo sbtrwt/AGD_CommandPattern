0c31f4810a7d3adb0ad11ceda2b07de81a08fcea
mridul-outscal <mridul.agrawal@outscal.com>
2023-07-06 17:57:40 +0530

    Updated UIService

--- Assets/Scripts/UI/UIService.cs
+++ Assets/Scripts/UI/UIService.cs
@@ -1,17 +1,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Command.Commands;
+using Command.Main;
 
 namespace Command.UI
 {
     public class UIService : MonoBehaviour
     {
         private BattleSelectionController battleSelectionController;
+        [SerializeField] private BattleSelectionView battleSelectionView;
+        [SerializeField] private BattleButtonView battleButtonPrefab;
 
+        private void Start()
+        {
+            battleSelectionController = new BattleSelectionController(battleSelectionView, battleButtonPrefab);
+        }
 
-
-
-
+        public void ShowBattleSelection(int battleCount) => battleSelectionController.Show(battleCount);
 
         public void ConfigureCommandSelectionUI(List<CommandType> executableCommands)
         {
