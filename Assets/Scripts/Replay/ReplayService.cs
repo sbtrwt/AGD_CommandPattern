a850699acf4f961afe02edc260149a65cac3f210
mridul-outscal <mridul.agrawal@outscal.com>
2023-07-08 03:33:01 +0530

    Updated Replay Service

--- Assets/Scripts/Replay/ReplayService.cs
+++ Assets/Scripts/Replay/ReplayService.cs
@@ -6,6 +6,16 @@ namespace Command.Replay
 {
     public class ReplayService
     {
+        public ReplayState ReplayState { get; private set; }
 
+        public ReplayService() => SetReplayState(ReplayState.DEACTIVE);
+
+        private void SetReplayState(ReplayState stateToSet) => ReplayState = stateToSet;
+    }
+
+    public enum ReplayState
+    {
+        ACTIVE,
+        DEACTIVE
     }
 }
