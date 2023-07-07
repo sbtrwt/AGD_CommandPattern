0f56fe3748a08e462ea517111fd5525ef729b7d0
mridul-outscal <mridul.agrawal@outscal.com>
2023-07-08 03:31:10 +0530

    Updated UIService

--- Assets/Scripts/UI/UIService.cs
+++ Assets/Scripts/UI/UIService.cs
@@ -7,25 +7,46 @@ namespace Command.UI
 {
     public class UIService : MonoBehaviour
     {
-        private BattleSelectionController battleSelectionController;
-        [SerializeField] private BattleSelectionView battleSelectionView;
+        // Battle Selection UI:
+        private BattleSelectionUIController battleSelectionController;
+        [SerializeField] private BattleSelectionUIView battleSelectionView;
         [SerializeField] private BattleButtonView battleButtonPrefab;
+
+        // Gameplay UI:
+        private GameplayUIController gameplayController;
+        [SerializeField] private GameplayUIView gameplayView;
+
+        private ActionSelectionUIController actionSelectionController;
+        [SerializeField] private ActionSelectionUIView actionSelectionView;
+        [SerializeField] private ActionButtonView actionButtonPrefab;
 
         private void Start()
         {
-            battleSelectionController = new BattleSelectionController(battleSelectionView, battleButtonPrefab);
+            battleSelectionController = new BattleSelectionUIController(battleSelectionView, battleButtonPrefab);
+            gameplayController = new GameplayUIController(gameplayView);
+            actionSelectionController = new ActionSelectionUIController(actionSelectionView, actionButtonPrefab);
         }
 
-        public void ShowBattleSelection(int battleCount) => battleSelectionController.Show(battleCount);
+        public void ShowBattleSelectionView(int battleCount) => battleSelectionController.Show(battleCount);
 
-        public void ConfigureCommandSelectionUI(List<CommandType> executableCommands)
+        public void ShowGameplayView(Sprite backgroundImage)
         {
-            // Configure Action Selection Buttons inisde a vertical layout group.
+            gameplayController.Show();
+            gameplayController.SetBackgroundImage(backgroundImage);
         }
 
-        public void OnActionSelected(CommandType selectedAction)
+        public void ShowActionSelectionView(List<CommandType> executableActions)
         {
-
+            switch(GameService.Instance.ReplayService.ReplayState)
+            {
+                case Replay.ReplayState.ACTIVE:
+                    // Tell the Replay Service to execute the next unit action command in the queue.
+                    break;
+                case Replay.ReplayState.DEACTIVE:
+                    actionSelectionController.Show(executableActions);
+                    break;
+            }
         }
+
     }
 }
