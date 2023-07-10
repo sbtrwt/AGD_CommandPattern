077316c5c5057e415faff219c5d4f6cec2fc933f
mridul-outscal <mridul.agrawal@outscal.com>
2023-07-10 07:12:11 +0530

    Implemented Attack Action

--- Assets/Scripts/Action/AttackAction.cs
+++ Assets/Scripts/Action/AttackAction.cs
@@ -1,14 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
+using Command.Commands;
+using Command.Player;
 using UnityEngine;
 
 namespace Command.Action
 {
     public class AttackAction : IAction
     {
-        public void PerformAction(int actorUnitId, int targetUnitId)
+        public void PerformAction(UnitController actorUnit, UnitController targetUnit, bool successful)
         {
-            
+            actorUnit.PlayActionAnimation(CommandType.Attack);
+            if (successful)
+                targetUnit.TakeDamage(actorUnit.Power);
         }
     }
 }
