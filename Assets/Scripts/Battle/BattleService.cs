adeb81dc75102f7a1e2d17424102702f9eebb685
mridul-outscal <mridul.agrawal@outscal.com>
2023-07-10 14:01:08 +0530

    Updated Battle Service - background image and ReplayBattle()

--- Assets/Scripts/Battle/BattleService.cs
+++ Assets/Scripts/Battle/BattleService.cs
@@ -1,29 +1,47 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Command.Main;
+using UnityEngine.UI;
 
 namespace Command.Battle
 {
     public class BattleService
     {
         private List<BattleScriptableObject> battleScriptableObjects;
+        private Image backgroundImage;
+        private int currentBattleId;
 
-        public BattleService(List<BattleScriptableObject> battleScriptableObjects)
+        public BattleService(List<BattleScriptableObject> battleScriptableObjects, Image backgroundImage)
         {
             this.battleScriptableObjects = battleScriptableObjects;
+            this.backgroundImage = backgroundImage;
             SubscribeToEvents();
+            backgroundImage.gameObject.SetActive(false);
         }
 
-        private void SubscribeToEvents() => GameService.Instance.EventService.OnBattleSelected.AddListener(LoadBattle);
+        private void SubscribeToEvents()
+        {
+            GameService.Instance.EventService.OnBattleSelected.AddListener(LoadBattle);
+            GameService.Instance.EventService.OnReplayButtonClicked.AddListener(ReplayBattle);
+        }
+
+        private void ReplayBattle() => LoadBattle(currentBattleId);
 
         private void LoadBattle(int battleId)
         {
+            currentBattleId = battleId;
             var battleDataToLoad = GetBattleDataByID(battleId);
-            GameService.Instance.UIService.ShowGameplayView(battleDataToLoad.BattleBackgroundImage);
+            SetBackgroundImage(battleDataToLoad.BattleBackgroundImage);
+            GameService.Instance.UIService.ShowGameplayView();
             GameService.Instance.PlayerService.Init(battleDataToLoad.Player1Data, battleDataToLoad.Player2Data);
         }
 
         private BattleScriptableObject GetBattleDataByID(int battleId) => battleScriptableObjects.Find(battleSO => battleSO.BattleID == battleId);
+
+        private void SetBackgroundImage(Sprite bgSprite)
+        {
+            backgroundImage.gameObject.SetActive(true);
+            backgroundImage.sprite = bgSprite;
+        }
     }
 }
