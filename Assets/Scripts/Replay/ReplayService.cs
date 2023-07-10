18009ec28b0a0700e4f7c6a14d642fe00fa6f10f
mridul-outscal <mridul.agrawal@outscal.com>
2023-07-10 14:03:44 +0530

    Implemented ReplayService

--- Assets/Scripts/Replay/ReplayService.cs
+++ Assets/Scripts/Replay/ReplayService.cs
@@ -1,16 +1,21 @@
-using System.Collections;
+using Command.Commands;
+using Command.Main;
 using System.Collections.Generic;
-using UnityEngine;
 
 namespace Command.Replay
 {
     public class ReplayService
     {
+        private Stack<ICommand> replayCommandStack;
         public ReplayState ReplayState { get; private set; }
 
         public ReplayService() => SetReplayState(ReplayState.DEACTIVE);
 
-        private void SetReplayState(ReplayState stateToSet) => ReplayState = stateToSet;
+        public void SetReplayState(ReplayState stateToSet) => ReplayState = stateToSet;
+
+        public void SetCommandStack(Stack<ICommand> commandsToSet) => replayCommandStack = new Stack<ICommand>(commandsToSet);
+
+        public void ExecuteNext() => GameService.Instance.ProcessUnitCommand(replayCommandStack.Pop());
     }
 
     public enum ReplayState
