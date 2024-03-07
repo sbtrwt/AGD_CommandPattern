51d6ad70d99e2844398c6bdab993400eb6240d80
shobhit-outscal <shobhit.rawat@outscal.com>
2024-03-07 13:28:07 +0530

    Added command invoker

---
+++ Assets/Scripts/Command/CommandInvoker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command.Command
+{
+    public class CommandInvoker
+    {
+        // A stack to keep track of executed commands.
+        private Stack<ICommand> commandRegistry = new Stack<ICommand>();
+
+        /// <summary>
+        /// Process a command, which involves both executing it and registering it.
+        /// </summary>
+        /// <param name="commandToProcess">The command to be processed.</param>
+        public void ProcessCommand(ICommand commandToProcess)
+        {
+            ExecuteCommand(commandToProcess);
+            RegisterCommand(commandToProcess);
+        }
+
+        /// <summary>
+        /// Execute a command, invoking its associated action.
+        /// </summary>
+        /// <param name="commandToExecute">The command to be executed.</param>
+        public void ExecuteCommand(ICommand commandToExecute) => commandToExecute.Execute();
+
+        /// <summary>
+        /// Register a command by adding it to the command registry stack.
+        /// </summary>
+        /// <param name="commandToRegister">The command to be registered.</param>
+        public void RegisterCommand(ICommand commandToRegister) => commandRegistry.Push(commandToRegister);
+    }
+}
