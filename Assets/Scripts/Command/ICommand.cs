a940f05b5a52c1f16ad692790c048e6d37cb2757
shobhit-outscal <shobhit.rawat@outscal.com>
2024-03-07 13:15:04 +0530

    Added ICommand and UnitCommand

---
+++ Assets/Scripts/Command/ICommand.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Command.Command
+{
+
+    public interface ICommand 
+    {
+        void Execute();
+    }
+}
---
+++ Assets/Scripts/Command/UnitCommand.cs
@@ -0,0 +1,33 @@
+﻿using Command.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command.Command
+{
+    public abstract class UnitCommand:ICommand
+    {
+        // Fields to store information related to the command.
+        public int ActorUnitID;
+        public int TargetUnitID;
+        public int ActorPlayerID;
+        public int TargetPlayerID;
+
+        // References to the actor and target units, accessible by subclasses.
+        protected UnitController actorUnit;
+        protected UnitController targetUnit;
+
+        /// <summary>
+        /// Abstract method to execute the unit command. Must be implemented by concrete subclasses.
+        /// </summary>
+        public abstract void Execute();
+
+        /// <summary>
+        /// Abstract method to determine whether the command will successfully hit its target.
+        /// Must be implemented by concrete subclasses.
+        /// </summary>
+        public abstract bool WillHitTarget();
+    }
+}
