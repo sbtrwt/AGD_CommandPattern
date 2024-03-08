a8994596a66b94533413367893c1b50404b67f60
shobhit-outscal <shobhit.rawat@outscal.com>
2024-03-08 20:51:21 +0530

    Added undo in command invoker

--- Assets/Scripts/Command/CommandInvoker.cs
+++ Assets/Scripts/Command/CommandInvoker.cs
@@ -16,5 +16,16 @@ namespace Command.Commands
         public void ExecuteCommand(ICommand commandToExecute) => commandToExecute.Execute();
 
         public void RegisterCommand(ICommand commandToRegister) => commandRegistry.Push(commandToRegister);
+        private bool RegistryEmpty() => commandRegistry.Count == 0;
+
+        private bool CommandBelongsToActivePlayer()
+        {
+           return (commandRegistry.Peek() as UnitCommand).commandData.ActorPlayerID == GameService.Instance.PlayerService.ActivePlayerID;
+        }
+        public void Undo()
+        {
+            if (!RegistryEmpty() && CommandBelongsToActivePlayer())
+                commandRegistry.Pop().Undo();
+        }
     }
 }
